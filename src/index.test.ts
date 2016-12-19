4775f07b2f822d98c19d5db6440e2aa7ff58e22b
Johan Borestad <johan.borestad@netent.com>
2016-12-19 18:29:26 +0100

    Add spread example for Typescript 2.1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,26 @@
-import { expect } from 'chai'
-import { add } from './'
+import { add, spread } from './'
 import test from 'ava'
 
-test('foo', t => {
+test('should pass', t => {
   t.pass()
 })
 
-test('bar', async t => {
+test('should resolve async methods', async t => {
   t.plan(1)
   const bar = Promise.resolve('bar')
   t.is(await bar, 'bar')
 })
 
-test('baz', t => {
+test('.add()', t => {
   const value = add(1,2)
   t.deepEqual(value, 3)
+})
+
+test('.spread() - should create a shallow object clone', t => {
+  const obj1 = { a: 'foo', b: 'bar' }
+  const obj2: any = spread(obj1)
+  obj2.a = 'baz'
+
+  t.not(obj1.a, obj2.a)
+  t.is(obj1.a, 'foo')
 })
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import {map, each} from 'lodash'
-
 export function add(a, b) {
   return (a + b)
 }
 
-export function spread() {
-  const original = {
-    a: 'foo',
-    b: 'bar'
-  }
-
-  let copy = { ...original }
+export function spread(obj: Object) {
+  let copy = { ...obj }
   return copy
 }
